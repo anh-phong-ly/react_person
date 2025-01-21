9ede799ad0329828ed08bb96527a45452281608b
PhongLy <ly.phong165@gmail.com>
2025-01-21 08:48:57 +0700

    fix test

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,12 +12,12 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
-      {(isMarried) && (
+      {isMarried && (
         <p className="Person__partner">
           {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
         </p>
       )}
-      {(!isMarried) && (<p className="Person__partner">I am not married</p>)}
+      {!isMarried && <p className="Person__partner">I am not married</p>}
     </section>
   );
 };
